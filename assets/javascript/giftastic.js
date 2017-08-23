4bd19cae5433bab608f5b9e3d99e9ec8dd1d3255
ryteone1 <rynlacy@yahoo.com>
2017-08-23 02:31:58 -0400

    progress

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  var topics = ["hillary clinton", "donald trump", "barack obama", "bernie sanders", "george w. bush"];
+
+  
+    function showPoliticianButtons() {
+    $("#politicianButtons").empty();
+      for (var i = 0; i < topics.length; i++) {
+        console.log(topics);
+        var newButton = $("<button>");
+            newButton.addClass("politician");
+            newButton.attr("data-name", topics[i]);
+            newButton.text(topics[i]);
+
+    $("#politicianButtons").append(newButton);
+    }
+    }
+    showPoliticianButtons();
+});
+
+
+
+
+// Event listener for all button elements
+    $("button").on("click", function() {
+      // In this case, the "this" keyword refers to the button that was clicked
+      var person = $(this).attr("data-person");
+
+      // Constructing a URL to search Giphy for the name of the person who said the quote
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=55fa83da04e04a38b28a997d9d79f784&limit=10";
+
+
+
+      // Performing our AJAX GET request
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        // After the data comes back from the API
+        .done(function(response) {
+          console.log(response);
+          // Storing an array of results in the results variable
+          var results = response.data;
+
+          // Looping over every result item
+          for (var i = 0; i < results.length; i++) {
+
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              // Creating a div with the class "item"
+              var gifDiv = $("<div class='item'>");
+
+              // Storing the result item's rating
+              var rating = results[i].rating;
+
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + rating);
+
+              // Creating an image tag
+              var personImage = $("<img>");
+
+              // Giving the image tag an src attribute of a proprty pulled off the
+              // result item
+              personImage.attr("src", results[i].images.fixed_height.url);
+
+              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+              $("#gifs-appear-here").prepend(gifDiv);
+            }
+          }
+        });
+    });
+
+    $("button").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
